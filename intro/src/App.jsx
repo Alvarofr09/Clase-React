5bda8b68f2670d454693de6b092b3086bd27e2eb
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-03-11 09:49:29 +0100

    Quitar los imports no usados

--- intro/src/App.jsx
+++ intro/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Saludo from "./components/Saludo";
 import Releevant from "./components/Releevant";
 import goku from "./assets/goku.png";
