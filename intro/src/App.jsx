e266016e04de175c76af58963b04136023bcf734
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-03-11 09:24:46 +0100

    Importar las cosas y usarlos

--- intro/src/App.jsx
+++ intro/src/App.jsx
@@ -1,21 +1,18 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import Welcome from "./components/Welcome";
+import Saludo from "./components/Saludo";
+import Releevant from "./components/Releevant";
+import goku from "./assets/goku.png";
 import "./App.css";
 
 function App() {
 	return (
 		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<Welcome />
+			<Saludo />
+			<img src={goku} alt="Goku" />
+			<Saludo />
+			<Releevant />
 		</>
 	);
 }
