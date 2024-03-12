0ecba62c7aa93dca0a063473faba4a91fe6d4de4
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-03-12 09:50:47 +0100

    Usar el componente card con props

--- intro/src/App.jsx
+++ intro/src/App.jsx
@@ -1,15 +1,25 @@
 import Saludo from "./components/Saludo";
-// import goku from "./assets/goku.png";
+import goku from "./assets/goku.png";
+import Vegeta from "./assets/Vegeta.jpg";
 import Card from "./components/Card";
 import "./App.css";
 
 function App() {
 	return (
 		<>
-			<Saludo />
-			<img src={goku} alt="Goku" className="Goku" />
-			<Saludo />
-			<Card />
+			<Saludo name="Alvaro" />
+			{/* <img src={goku} alt="Goku" className="Goku" /> */}
+			<Saludo name="Pepe" />
+			<Card
+				src={goku}
+				title="Goku"
+				descripcion="Esta es una imagen de goku de GT"
+			/>
+			<Card
+				src={Vegeta}
+				title="Vegeta"
+				descripcion="Este panel de manga es de cuando vegeta uso el ultraego"
+			/>
 			<h2>Releevant :)</h2>
 		</>
 	);
