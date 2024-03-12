e3b448ac1b5924f7ba88c14ca3152ec98ec2524c
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-03-12 09:50:29 +0100

    Como hacer la card con los props

--- intro/src/components/Card.jsx
+++ intro/src/components/Card.jsx
@@ -1,17 +1,14 @@
 import goku from "../assets/goku.png";
 
-export default function Card() {
+export default function Card(props) {
 	return (
 		<div className="card" style={{ width: "18rem" }}>
-			<img src={goku} className="card-img-top" alt="Goku" />
+			<img src={props.src} className="card-img-top" alt={props.title} />
 			<div className="card-body">
-				<h5 className="card-title">Goku</h5>
-				<p className="card-text">
-					Some quick example text to build on the card title and make up the
-					bulk of the card's content.
-				</p>
+				<h5 className="card-title">{props.title}</h5>
+				<p className="card-text">{props.descripcion}</p>
 				<a href="#" className="btn btn-primary">
-					Click aquí
+					Ver detalles
 				</a>
 			</div>
 		</div>
