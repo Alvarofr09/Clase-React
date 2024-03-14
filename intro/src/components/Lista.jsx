235aea2a0f43a02dc30f37fe0dfa36fb7e35c88c
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-03-14 10:44:42 +0100

    Hacer la ternaria

--- intro/src/components/Lista.jsx
+++ intro/src/components/Lista.jsx
@@ -3,13 +3,19 @@ export default function Lista({ title, items }) {
 		<>
 			<h1>Lista de {title}</h1>
 			<ul>
-				{items.map((item) => {
-					if (typeof item === "object") {
-						return <li key={item.id}>{item.name}</li>;
-					} else {
-						return <li key={item}>{item}</li>;
-					}
-				})}
+				{items.map((item) =>
+					// if (typeof item === "object") {
+					// 	return <li key={item.id}>{item.name}</li>;
+					// } else {
+					// 	return <li key={item}>{item}</li>;
+					// }
+
+					typeof item === "object" ? (
+						<li key={item.id}>{item.name}</li>
+					) : (
+						<li key={item}>{item}</li>
+					)
+				)}
 			</ul>
 		</>
 	);
