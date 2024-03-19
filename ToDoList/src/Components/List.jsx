de92f11b2e6e13f41b0501d0a9125697e6524850
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-03-19 13:22:31 +0100

    Empezar con la implementacion del toggle

--- ToDoList/src/Components/List.jsx
+++ ToDoList/src/Components/List.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+
 export default function List({ items }) {
+	const [isCompleted, setIsCompleted] = useState("false");
+
+	function handleClick(e) {
+		setIsCompleted(!isCompleted);
+	}
 	return (
 		<>
 			<ul>
 				{items.map((item) =>
 					typeof item === "object" ? (
-						<li key={item.id}>{item.name}</li>
+						<li
+							className={isCompleted ? "completed" : null}
+							onClick={handleClick}
+							key={item.id}
+						>
+							{item.name}
+						</li>
 					) : (
-						<li key={item}>{item}</li>
+						<li
+							className={isCompleted ? "completed" : null}
+							onClick={handleClick}
+							key={item}
+						>
+							{item}
+						</li>
 					)
 				)}
 			</ul>
