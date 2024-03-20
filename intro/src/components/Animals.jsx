d91d1e0eeaf8352305a62d4bb49d6a6ed7d33fb3
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-03-20 11:04:56 +0100

    Hacer para que se puedan borrar animales

--- intro/src/components/Animals.jsx
+++ intro/src/components/Animals.jsx
@@ -2,11 +2,21 @@ import { useState } from "react";
 
 import Lista from "./Lista";
 import Form from "./Form";
+import Select from "./Select";
 
 const animalsArray = ["Perro", "Gato", "Conejo"];
 
 export default function Animals() {
 	const [animals, setAnimals] = useState(animalsArray);
+
+	function deleteAnimal() {
+		// Hacer copia del array
+		const newAnimals = [...animals];
+		// Excluir el elemento cliclado
+		const restAnimals = newAnimals.filter((animal) => animal !== "Conejo");
+		// Volver a setear el array modificado
+		setAnimals(restAnimals);
+	}
 
 	return (
 		<>
@@ -18,6 +28,12 @@ export default function Animals() {
 				placeholder="Escribe tu animal aqui"
 				buttonText="Añadir animal"
 			/>
+			<button onClick={deleteAnimal}>Eliminar Conejo</button>
+			<Select>
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+			</Select>
 		</>
 	);
 }
