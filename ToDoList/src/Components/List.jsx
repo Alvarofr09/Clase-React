13b6d6e977924a3351705957e4c320fa6da6a286
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-03-20 11:06:35 +0100

    Seguir con el To Do

--- ToDoList/src/Components/List.jsx
+++ ToDoList/src/Components/List.jsx
@@ -1,39 +1,39 @@
 import { useState } from "react";
 
-export default function List({ items }) {
-	// Creamos un estado para almacenar los estados de los items
-	const [completedItems, setCompletedItems] = useState({});
+import Button from "./Button";
 
-	// Función para manejar el click a un ítem
+export default function List({ items, setState }) {
+	// Creamos un estado para almacenar los estados de los items
+	// const [completedItems, setCompletedItems] = useState({});
+	// const [isCompleted, setIsCompleted] = useState(false);
+
 	function handleClick(id) {
-		// Actualizamos el estado de completado para el item correspondiente
-		setCompletedItems((prevCompletedItems) => ({
-			...prevCompletedItems,
-			[id]: !prevCompletedItems[id], // Invertimos el estado de completado
-		}));
+		const itemsCopy = [...items];
+		const updateItem = itemsCopy.find((item) => item.id === id);
+		updateItem.isCompleted = !updateItem.isCompleted;
+		console.log(updateItem);
+
+		setState(updateItem);
 	}
+
+	// function deleteTask(id) {
+	// 	const newItems = items.filter((item) => item.id !== id);
+	// 	setIsCompleted(newItems);
+	// }
+
 	return (
 		<>
 			<ul>
-				{items.map((item) =>
-					typeof item === "object" ? (
-						<li
-							className={completedItems[item.id] ? "completed" : null}
-							onClick={() => handleClick(item.id)}
-							key={item.id}
-						>
-							{item.name}
-						</li>
-					) : (
-						<li
-							className={completedItems[item] ? "completed" : null}
-							onClick={() => handleClick(item)}
-							key={item}
-						>
-							{item}
-						</li>
-					)
-				)}
+				{items.map((item) => (
+					<li
+						className={item.isCompleted ? "completed" : null}
+						onClick={() => handleClick(item.id)}
+						key={item.id}
+					>
+						{item.name}{" "}
+						{/* <Button text="Delete" onClick={() => deleteTask(item.id)} /> */}
+					</li>
+				))}
 			</ul>
 		</>
 	);
