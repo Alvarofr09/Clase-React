b11ab08cc225d43caab7016f6246d765a4431729
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-03-22 11:45:15 +0100

    BrowserRouter, Routes y Route

--- PruebaRouter/src/main.jsx
+++ PruebaRouter/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import Home from "./Views/Home.jsx";
+import Contact from "./Views/Contact.jsx";
+import About from "./Views/About.jsx";
+import NotFound from "./Views/NotFound.jsx";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />}>
+					<Route index element={<Home />} />
+					<Route path="contact" element={<Contact />} />
+					<Route path="about" element={<About />} />
+					<Route path="*" element={<NotFound />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
+);
