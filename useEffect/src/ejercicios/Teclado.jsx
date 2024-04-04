c0da7dd75c63e41dbcc8b23db954830b66b800a9
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-04 10:37:43 +0200

    Componente teclado

---
+++ useEffect/src/ejercicios/Teclado.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function Teclado() {
+	const [tecla, setTecla] = useState("");
+
+	useEffect(() => {
+		document.addEventListener("keydown", (e) => {
+			setTecla(e.key);
+		});
+
+		return () => {
+			document.removeEventListener("keydown", (e) => {
+				setTecla(e.key);
+			});
+		};
+	});
+
+	return (
+		<div>
+			<p>{tecla}</p>
+		</div>
+	);
+}
