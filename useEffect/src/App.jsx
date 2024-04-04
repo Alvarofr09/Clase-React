425e0d3731e86ac4722871615457747d1cba30a8
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-04 10:37:23 +0200

    Usar componentes

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -1,26 +1,27 @@
-import { useState } from "react";
-import TodoList from "./components/TodoList";
-import Users from "./components/Users";
-import Scroll from "./components/Scroll";
-import Developer from "./components/Developer";
+// import { useState } from "react";
+// import TodoList from "./components/TodoList";
+// import Users from "./components/Users";
+// import Scroll from "./components/Scroll";
+// import Developer from "./components/Developer";
+import Hora from "./ejercicios/Hora";
+import Teclado from "./ejercicios/Teclado";
 
 /**
  * Ha diferencia de los datos simples, los complejos se guardan en memoria y se guardan por referencia y no por valor
  * Cuando se renderiza el componente de nuevo, la referencia del objeto cambia y es como si fuera uno nuevo
  * Pero sacandolo fuera del componente ya no se crea de nuevo, por lo cual ya no se crea de nuevo
  */
-const developer = {
-	name: "Alvaro",
-};
+// const developer = {
+// 	name: "Alvaro",
+// };
 
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
 	return (
 		<>
-			<Developer developer={developer} />
-			<button onClick={() => setCount(count + 1)}>Incrementar</button>
+			<Hora />
+			<Teclado />
 		</>
 	);
 }
