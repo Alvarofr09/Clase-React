4ebb00363abbf2b60c36ac59e3b2b65f4befbc94
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-08 11:08:39 +0200

    Solo mostar home

--- useContext/src/App.jsx
+++ useContext/src/App.jsx
@@ -1,12 +1,18 @@
 import Home from "./views/Home";
 import "./App.css";
 import AuthContextProvider from "./contexts/AuthContext";
+import CountContextProvider from "./contexts/CountContext";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
 	return (
-		<AuthContextProvider>
-			<Home />
-		</AuthContextProvider>
+		<ThemeContextProvider>
+			<AuthContextProvider>
+				<CountContextProvider>
+					<Home />
+				</CountContextProvider>
+			</AuthContextProvider>
+		</ThemeContextProvider>
 	);
 }
 
