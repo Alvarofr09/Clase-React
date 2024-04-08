e42155bdac5c4273a5f8c31a2ed9bd9f4aee48e1
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-08 09:57:39 +0200

    Cambiar un poco la logica

--- useEffect/src/ejercicios/Modal.jsx
+++ useEffect/src/ejercicios/Modal.jsx
@@ -1,27 +1,38 @@
 import { useEffect, useState } from "react";
+import "./Modal.css";
 
 export default function Modal() {
 	const [showModal, setShowModal] = useState(false);
 
 	useEffect(() => {
-		const modal = document.querySelector(".modal");
-		if (!showModal) {
-			modal.style.display = "none";
+		const body = document.body;
+		body.style.overflow = !showModal ? "scroll" : "hidden";
+		if (showModal) {
+			body.classList.add("modal-overlay");
 		} else {
-			modal.style.display = "block";
+			body.classList.remove("modal-overlay");
 		}
 	}, [showModal]);
+
+	function toggleModal() {
+		setShowModal(!showModal);
+	}
 	return (
 		<div>
 			<h1>Modal</h1>
-			<button onClick={() => setShowModal(true)}>Abrir Modal</button>
+			<button onClick={toggleModal}>Abrir Modal</button>
 
 			<div
 				className="modal"
-				style={{ width: "100px", height: "100px", backgroundColor: "red" }}
+				style={{
+					width: "100px",
+					height: "100px",
+					backgroundColor: "red",
+					display: showModal ? "block" : "none",
+				}}
 			>
 				<p>Contenido Modal</p>
-				<button onClick={() => setShowModal(false)}>Cerrar modal</button>
+				<button onClick={toggleModal}>Cerrar modal</button>
 			</div>
 		</div>
 	);
