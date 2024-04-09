85de5550f0faf992f951a205981327801476a74c
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-09 09:32:11 +0200

    Quitar cosas innecesarias

--- useContext/src/ejercicios/Theme.jsx
+++ useContext/src/ejercicios/Theme.jsx
@@ -1,14 +1,9 @@
-import { useThemeContext } from "../contexts/ThemeContext";
-
 import ThemeSwitch from "./ThemeSwitch";
 
 export default function Theme() {
-	const { theme } = useThemeContext();
-
 	return (
-		<div className={theme === "light" ? "light-theme" : "dark-theme"}>
-			<h1>Current theme: {theme}</h1>
+		<>
 			<ThemeSwitch />
-		</div>
+		</>
 	);
 }
