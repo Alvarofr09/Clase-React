c28e583e02c86f9b6e3abccacb1a622d62b5ff8c
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-09 11:03:04 +0200

    refactorizar un poco del codigo

--- useContext/src/views/Home.jsx
+++ useContext/src/views/Home.jsx
@@ -1,7 +1,3 @@
-// Esta es la version que hemos remplazado, que te tienes que importar tanto el uso del contexto como el contexto.
-// import { useContext } from "react";
-// import { AuthContext } from "../contexts/AuthContext";
-
 // Importamos la funcion que nos da el contexto
 import { useEffect } from "react";
 import { useAuthContext } from "../contexts/AuthContext";
@@ -11,24 +7,14 @@ import Theme from "../ejercicios/Theme";
 import Gallery from "../ejercicios/Gallery";
 
 export default function Home() {
-	// Esta es la version que hemos remplazado, que tienes que usar tanto el uso del contexto como el contexto.
-	// const { userInfo } = useContext(AuthContext);
-
-	// Y aquí en vez de usar el contexto directamente y ponerle el contexto dentro,
-	// usamos la funcion que nos usa el cotexto directamente
 	const { userInfo } = useAuthContext();
-	const { darkMode } = useThemeContext();
+	const { isDarkMode } = useThemeContext();
 
 	useEffect(() => {
 		const body = document.body;
-		if (darkMode) {
-			body.classList.remove("light-theme");
-			body.classList.add("dark-theme");
-		} else {
-			body.classList.remove("dark-theme");
-			body.classList.add("light-theme");
-		}
-	}, [darkMode]);
+		body.className = isDarkMode ? "dark-theme" : "light-theme";
+	}, [isDarkMode]);
+
 	return (
 		<>
 			<h1>Home</h1>
