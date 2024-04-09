74463ed7d3aa821b603b2d0283e72b2d1ce1e89a
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-09 11:02:36 +0200

    contextos

--- useContext/src/App.jsx
+++ useContext/src/App.jsx
@@ -1,18 +1,22 @@
 import Home from "./views/Home";
 
 import "./App.css";
-// import AuthContextProvider from "./contexts/AuthContext";
-// import CountContextProvider from "./contexts/CountContext";
+import AuthContextProvider from "./contexts/AuthContext";
+import CountContextProvider from "./contexts/CountContext";
 import ThemeContextProvider from "./contexts/ThemeContext";
 import GalleryContextProvider from "./contexts/GaleryContext";
 
 function App() {
 	return (
-		<ThemeContextProvider>
-			<GalleryContextProvider>
-				<Home />
-			</GalleryContextProvider>
-		</ThemeContextProvider>
+		<AuthContextProvider>
+			<CountContextProvider>
+				<ThemeContextProvider>
+					<GalleryContextProvider>
+						<Home />
+					</GalleryContextProvider>
+				</ThemeContextProvider>
+			</CountContextProvider>
+		</AuthContextProvider>
 	);
 }
 
