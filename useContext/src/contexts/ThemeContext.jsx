c228f26f6b5f30e265f027d01e5d5836d690fbae
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-09 09:31:59 +0200

    cambiar la logica

--- useContext/src/contexts/ThemeContext.jsx
+++ useContext/src/contexts/ThemeContext.jsx
@@ -1,14 +1,19 @@
 import { createContext, useContext, useState } from "react";
 
 export const ThemeContext = createContext({
-	theme: "",
-	setTheme: () => {},
+	darkMode: false,
+	setDarkMode: () => {},
 });
 
 export default function ThemeContextProvider({ children }) {
-	const [theme, setTheme] = useState("dark");
+	const [darkMode, setDarkMode] = useState(false);
+
+	const toggleTheme = () => {
+		setDarkMode(!darkMode);
+	};
+
 	return (
-		<ThemeContext.Provider value={{ theme, setTheme }}>
+		<ThemeContext.Provider value={{ darkMode, setDarkMode, toggleTheme }}>
 			{children}
 		</ThemeContext.Provider>
 	);
