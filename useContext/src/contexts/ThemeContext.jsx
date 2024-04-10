cd7411a1428e361bab81bb041333a1a769c4984d
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-10 09:59:04 +0200

    nuevo proyecto

--- useContext/src/contexts/ThemeContext.jsx
+++ useContext/src/contexts/ThemeContext.jsx
@@ -6,14 +6,23 @@ export const ThemeContext = createContext({
 });
 
 export default function ThemeContextProvider({ children }) {
+	const localTheme = localStorage.getItem("isDarkMode");
+	console.log(localTheme);
 	const prefersMode = window.matchMedia("(prefers-color-scheme: dark)").matches
 		? true
 		: false;
 
 	const [isDarkMode, setisDarkMode] = useState(prefersMode);
 
+	// if (localTheme) {
+	// 	setisDarkMode(localTheme);
+	// } else {
+	// 	setisDarkMode(prefersMode);
+	// }
+
 	const toggleTheme = () => {
 		setisDarkMode(!isDarkMode);
+		localStorage.setItem("isDarkMode", !isDarkMode);
 	};
 
 	return (
