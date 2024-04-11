dce4287b39895c16738d3fd31af3aeb95f7cea24
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-11 11:05:56 +0200

    Login hecho

---
+++ rutas-protegidas/src/views/Login.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { useAuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+	const { Login, errorMesagge } = useAuthContext();
+	const navigate = useNavigate();
+
+	const [user, setUser] = useState({ email: "", password: "" });
+
+	function handleInput(e) {
+		setUser({ ...user, [e.target.name]: [e.target.value] });
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		Login(user);
+		navigate("/");
+	}
+
+	return (
+		<div>
+			<h1>Login</h1>
+			<form action="" onSubmit={handleSubmit}>
+				<input
+					type="text"
+					id="email"
+					name="email"
+					onChange={handleInput}
+					value={user.email}
+					placeholder="email"
+					required
+				/>
+				<input
+					type="password"
+					id="password"
+					name="password"
+					onChange={handleInput}
+					value={user.password}
+					placeholder="Password"
+					required
+				/>
+				<p>{errorMesagge}</p>
+				<button>Log in</button>
+			</form>
+		</div>
+	);
+}
