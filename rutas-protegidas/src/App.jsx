ffb6ea551117205cecd6dd9e715feb5a73d43b28
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-11 11:04:20 +0200

    Hacer las rutas

--- rutas-protegidas/src/App.jsx
+++ rutas-protegidas/src/App.jsx
@@ -1,7 +1,32 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+import Login from "./views/Login";
+import About from "./views/About";
+import Profile from "./views/Profile";
+import Missing from "./views/Missing";
+import Layout from "./components/Layout";
+import RequireAuth from "./components/RequireAuth";
 
 function App() {
-	return <></>;
+	return (
+		<Routes>
+			{/*Public Routes*/}
+			<Route path="/" element={<Layout />}>
+				<Route path="/login" element={<Login />} />
+
+				{/*Protected Routes*/}
+				<Route element={<RequireAuth />}>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/profile" element={<Profile />} />
+				</Route>
+
+				{/*Missing Route*/}
+				<Route path="*" element={<Missing />} />
+			</Route>
+		</Routes>
+	);
 }
 
 export default App;
