b0daa33679b18046a2cebf2b0f51aa82f919f477
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-11 11:04:45 +0200

    Rutas

--- rutas-protegidas/src/main.jsx
+++ rutas-protegidas/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import AuthContextProvider from "./context/AuthContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<AuthContextProvider>
+				<Routes>
+					<Route path="/" element={<App />} />
+				</Routes>
+			</AuthContextProvider>
+		</BrowserRouter>
+	</React.StrictMode>
+);
