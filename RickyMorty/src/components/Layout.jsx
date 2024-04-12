b2abd939c971ff2ff13d816393f7c2d66ba95341
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-12 10:18:40 +0200

    Layout

--- RickyMorty/src/components/Layout.jsx
+++ RickyMorty/src/components/Layout.jsx
@@ -1,3 +1,26 @@
-// import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import { useAuthContext } from "../context/AuthContext";
 
-export default function Layout() {}
+export default function Layout() {
+	const { auth, logout } = useAuthContext();
+
+	return (
+		<div>
+			<nav>
+				<h1>Ricky and Morty</h1>
+				{auth && (
+					<>
+						<Link className="link" to="/">
+							Personajes
+						</Link>
+						<button onClick={logout}>Logout</button>
+					</>
+				)}
+			</nav>
+
+			<main className="app">
+				<Outlet />
+			</main>
+		</div>
+	);
+}
