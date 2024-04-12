4b9023fab78d6bac75e5e6cf07c2ff602afe19ba
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-12 10:19:31 +0200

    detalles de personajes

--- RickyMorty/src/views/Detalles.jsx
+++ RickyMorty/src/views/Detalles.jsx
@@ -1,5 +1,45 @@
-import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams, Link } from "react-router-dom";
 
 export default function Detalles() {
-	return <div>Detalles</div>;
+	const [character, setCharacter] = useState({});
+	const { id } = useParams();
+	let prevId = parseInt(id) - 1;
+	let nextId = parseInt(id) + 1;
+	console.log(id);
+
+	useEffect(() => {
+		async function fetchApi() {
+			let response = await fetch(
+				`https://rickandmortyapi.com/api/character/${id}`
+			);
+			let json = await response.json();
+			setCharacter(json);
+		}
+		fetchApi();
+	}, [id]);
+
+	return (
+		<div className="container">
+			<div className="">
+				<h1>{character.name}</h1>
+				<img src={character.image} alt={character.name} />
+				<p>Status: {character.status}</p>
+				<p>Species: {character.species}</p>
+				<p>Gender: {character.gender}</p>
+				<p>Origin: {character.origin?.name} </p>
+				<p>Location: {character.location?.name}</p>
+			</div>
+
+			<div className="">
+				<Link className="link" to={`/character/${prevId}`}>
+					Anterior
+				</Link>
+				<Link className="link" to={`/character/${nextId}`}>
+					Siguiente
+				</Link>
+			</div>
+		</div>
+	);
 }
