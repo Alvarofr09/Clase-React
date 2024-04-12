c06af6cf18d3476dc82e4ea53e284de83d022282
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-12 10:20:27 +0200

    Mostrar personajes

--- RickyMorty/src/views/Personajes.jsx
+++ RickyMorty/src/views/Personajes.jsx
@@ -1,5 +1,37 @@
-import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Personajes() {
-	return <div>Personajes</div>;
+	const [personajes, setPersonajes] = useState([]);
+
+	useEffect(function () {
+		async function fetchApi() {
+			let response = await fetch(`https://rickandmortyapi.com/api/character`);
+			let json = await response.json();
+			json = json.results;
+			setPersonajes(json);
+		}
+		fetchApi();
+	}, []);
+
+	return (
+		<div className="container">
+			<h1>Rick and Morty</h1>
+			<div className="contenedor">
+				{personajes.map((character) => {
+					return (
+						<div key={character.id} className="card">
+							<h3>{character.name}</h3>
+							<img src={character.image} alt={character.name} width={"30%"} />
+							<p>Status: {character.status}</p>
+							<Link className="link" to={`/${character.id}`}>
+								Ver detalles
+							</Link>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
