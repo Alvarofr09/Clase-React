a0bd4107b0500c08eac47877bf90fb1724192a19
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-12 10:18:33 +0200

    Hacer las rutas

--- RickyMorty/src/App.jsx
+++ RickyMorty/src/App.jsx
@@ -2,13 +2,23 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
 import Login from "./views/Login";
+import Layout from "./components/Layout";
+import RequireAuth from "./components/RequireAuth";
+import Personajes from "./views/Personajes";
+import Detalles from "./views/Detalles";
 
 function App() {
 	return (
 		<>
-			<div>App</div>
 			<Routes>
-				<Route path="/" element={<Login />} />
+				<Route path="/" element={<Layout />}>
+					<Route path="/login" element={<Login />} />
+
+					<Route element={<RequireAuth />}>
+						<Route path="/" element={<Personajes />} />
+						<Route path="/:id" element={<Detalles />} />
+					</Route>
+				</Route>
 			</Routes>
 		</>
 	);
