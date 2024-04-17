7680de69cf2f50f2bea67f522e27c8fcb3cbc07e
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-17 09:54:03 +0200

    traer componente

--- RickyMorty/src/views/Login.jsx
+++ RickyMorty/src/views/Login.jsx
@@ -1,49 +1,10 @@
-import { useState } from "react";
-import { useAuthContext } from "../context/AuthContext";
-import { useNavigate } from "react-router-dom";
-// import BasicForm from "../components/BasicForm/BasicForm";
+import LoginForm from "../components/LoginForm/LoginForm";
 
 export default function Login() {
-	const { login, errorMesagge } = useAuthContext();
-	const navigate = useNavigate();
-
-	const [user, setUser] = useState({ email: "", password: "" });
-
-	function handleInput(e) {
-		setUser({ ...user, [e.target.name]: e.target.value });
-	}
-
-	function handleSubmit(e) {
-		e.preventDefault();
-		login(user);
-		navigate("/");
-	}
-
 	return (
 		<div>
 			<h1>Login</h1>
-			<form action="" onSubmit={handleSubmit}>
-				<input
-					type="text"
-					id="email"
-					name="email"
-					onChange={handleInput}
-					value={user.email}
-					placeholder="email"
-					required
-				/>
-				<input
-					type="password"
-					id="password"
-					name="password"
-					onChange={handleInput}
-					value={user.password}
-					placeholder="Password"
-					required
-				/>
-				<p>{errorMesagge}</p>
-				<button>Log in</button>
-			</form>
+			<LoginForm />
 		</div>
 	);
 }
