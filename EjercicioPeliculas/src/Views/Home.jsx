1d934295e54a92cd47d2f31cbbe84055b2671999
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-19 11:01:59 +0200

    Empezar la busqueda de pelicula

--- EjercicioPeliculas/src/Views/Home.jsx
+++ EjercicioPeliculas/src/Views/Home.jsx
@@ -1,3 +1,11 @@
 export default function Home() {
-	return <div>Home</div>;
+	return (
+		<div className="flex flex-col justify-center items-center">
+			<h1>Busca tu pelicula</h1>
+			<form action="" className="flex flex-col">
+				<input type="text" placeholder="Pelicula" className="border" />
+				<button>Buscar</button>
+			</form>
+		</div>
+	);
 }
