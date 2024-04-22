f5e93e1c0de3acc3bef66b88c570cc5a9bc95c56
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-04-22 10:40:40 +0200

    Que traiga la pelicula buscada

--- EjercicioPeliculas/src/Views/Home.jsx
+++ EjercicioPeliculas/src/Views/Home.jsx
@@ -1,11 +1,45 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function Home() {
+	const [search, setSearch] = useState("");
+	const [pelicula, setPelicula] = useState({});
+	async function handleSubmit(event) {
+		event.preventDefault();
+		console.log(search);
+
+		let response = await fetch(
+			`http://www.omdbapi.com/?apikey=6bbc4e49&t=${search}`
+		);
+		let json = await response.json();
+		setPelicula(json);
+		setSearch("");
+	}
+
+	function handleChange(event) {
+		setSearch(event.target.value);
+	}
+
 	return (
 		<div className="flex flex-col justify-center items-center">
 			<h1>Busca tu pelicula</h1>
-			<form action="" className="flex flex-col">
-				<input type="text" placeholder="Pelicula" className="border" />
+			<form action="" className="flex flex-col" onSubmit={handleSubmit}>
+				<input
+					type="text"
+					value={search}
+					placeholder="Pelicula"
+					className="border"
+					onChange={handleChange}
+				/>
 				<button>Buscar</button>
 			</form>
+
+			<div key={pelicula.imdbID}>
+				<Link className="link" to={`/movies/${pelicula.imdbID}`}>
+					<img src={pelicula.Poster} alt={pelicula.Title} />
+					<h3>{pelicula.Title}</h3>
+				</Link>
+			</div>
 		</div>
 	);
 }
